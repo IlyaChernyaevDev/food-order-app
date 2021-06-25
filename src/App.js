86a7fa6f9d844194c925096a382d51d1d268c186
Ilya Chernyaev <ilyachernyaevdev@gmail.com>
2021-06-25 15:37:52 +0300

    added Modal component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import Header from './components/Layout/Header';
 import Meals from './components/Meals';
+import Cart from './components/Cart';
 
 function App() {
   return (
     <>
+      <Cart />
       <Header />
       <main>
         <Meals />
